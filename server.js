e41d22d24373e2d05a9e1bc9efd13379d350ffee
yhayes <hayesy@oregonstate.edu>
2025-02-07 14:19:03 -0800

    Added server.js for running Node.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3400;
+
+// Serve static files (HTML, CSS, JS) from the project directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html at the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://flip1.engr.oregonstate.edu:${PORT}/`);
+});
