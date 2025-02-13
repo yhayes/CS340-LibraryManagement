9855b43ef2e2a513066da5a5cb21c8cd087ed8e4
yhayes <hayesy@oregonstate.edu>
2025-02-13 11:43:54 -0800

    changed server again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 2134;
+const PORT = 5678;
 
 // Serve static files (HTML, CSS, JS) from the project directory
 app.use(express.static(path.join(__dirname)));
