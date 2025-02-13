fb1ae2c8856d71f7a474da7424e0988362f8abf4
yhayes <hayesy@oregonstate.edu>
2025-02-13 11:51:08 -0800

    trying something else in server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = 5678;
 
-// Serve static files (HTML, CSS, JS) from the project directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the cs340 directory
+app.use(express.static(__dirname));
 
-// Serve index.html at the root URL
+// Serve index.html explicitly
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
